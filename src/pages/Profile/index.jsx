972e464f2bf3abe849f518c53f5ffb79e229b16a
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-05-25 15:23:30 -0300

    feat : estrutura feita

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,7 +1,22 @@
+import "../../public/profile/profile.css";
+
 export const ProfilePage = () => {
-    return(
-        <>
-            profile
-        </>
-    )
-}
+    return (
+        <div className="container-fluid">
+            <div className="modal-group-profile-and-contacts">
+                <div className="profile" id="profile">
+                    PROFILE IS HERE
+                </div>
+                <div className="contacts" id="contacts">
+                    CONTACTS IS HERE
+                </div>
+            </div>
+            <div className="aboutme" id="aboutme">
+                ABOUT ME IS HERE
+            </div>
+            <div className="footer" id="footer">
+                FOOTER IS HERE
+            </div>
+        </div>
+    );
+};
